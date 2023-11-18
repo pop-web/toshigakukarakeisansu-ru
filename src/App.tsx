e7b4347a6e92554e6c2199eccc523b437571cea0
koga yusuke <popweb782@gmail.com>
2023-11-19 02:00:46 +0900

    計算ロジック

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,97 @@ import {
   ChakraProvider,
   Box,
   Button,
-  NumberInput,
-  NumberInputField,
   VStack,
   Text,
   InputRightAddon,
   InputGroup,
+  NumberInput,
+  NumberInputField,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
+type FormData = {
+  unitShares: number;
+  stockPrice: number;
+  investmentAmount: number;
+};
 
 const App = () => {
-  const [stockPrice, setStockPrice] = useState("");
-  const [investmentAmount, setInvestmentAmount] = useState("");
+  const { control, handleSubmit } = useForm<FormData>({
+    defaultValues: {
+      unitShares: 100,
+      stockPrice: 0,
+      investmentAmount: 0,
+    },
+  });
+  const [result, setResult] = useState("");
 
-  const handleCalculate = () => {
-    // ここに株数を計算するロジックを追加します
-    console.log("株価: ", stockPrice);
-    console.log("投資金額: ", investmentAmount);
+  const onSubmit = (data: FormData) => {
+    const { unitShares, stockPrice, investmentAmount } = data;
+
+    if (stockPrice > 0) {
+      const totalShares = Math.floor(investmentAmount / stockPrice);
+      const purchasableShares =
+        Math.floor(totalShares / unitShares) * unitShares;
+      setResult(`購入可能株数: ${purchasableShares} 株`);
+    } else {
+      setResult("株価を正しく入力してください。");
+    }
   };
+
   return (
     <ChakraProvider>
       <Box p={4}>
-        <VStack spacing={4}>
-          <Text fontSize="xl">投資金額から株数計算スール</Text>
-          <InputGroup justifyContent="center">
-            <NumberInput onChange={(valueString) => setStockPrice(valueString)}>
-              <NumberInputField placeholder="株価" />
-            </NumberInput>
-            <InputRightAddon children="円" />
-          </InputGroup>
-          <InputGroup justifyContent="center">
-            <NumberInput
-              onChange={(valueString) => setInvestmentAmount(valueString)}
-            >
-              <NumberInputField placeholder="投資金額" />
-            </NumberInput>
-            <InputRightAddon children="円" />
-          </InputGroup>
-          <Button colorScheme="blue" onClick={handleCalculate}>
-            計算
-          </Button>
-        </VStack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <VStack spacing={4}>
+            <Text fontSize="xl">投資金額から株数計算スール</Text>
+            <InputGroup justifyContent="center">
+              <Controller
+                name="unitShares"
+                control={control}
+                render={({ field }) => (
+                  <NumberInput defaultValue={100}>
+                    <NumberInputField {...field} placeholder="株数" />
+                  </NumberInput>
+                )}
+              />
+              <InputRightAddon children="株" />
+            </InputGroup>
+            <InputGroup justifyContent="center">
+              <Controller
+                name="stockPrice"
+                control={control}
+                render={({ field }) => (
+                  <NumberInput>
+                    <NumberInputField {...field} placeholder="株価" />
+                  </NumberInput>
+                )}
+              />
+              <InputRightAddon children="円" />
+            </InputGroup>
+            <InputGroup justifyContent="center">
+              <Controller
+                name="investmentAmount"
+                control={control}
+                render={({ field }) => (
+                  <NumberInput>
+                    <NumberInputField {...field} placeholder="投資金額" />
+                  </NumberInput>
+                )}
+              />
+              <InputRightAddon children="円" />
+            </InputGroup>
+            <Button colorScheme="blue" type="submit">
+              計算
+            </Button>
+          </VStack>
+        </form>
+        {result && (
+          <Text textAlign="center" mt={4}>
+            {result}
+          </Text>
+        )}
       </Box>
     </ChakraProvider>
   );
