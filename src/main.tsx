b972e08e251498acb6c60a07025013d48606e251
koga yusuke <popweb782@gmail.com>
2023-12-05 16:53:00 +0900

    一旦コメントアウト

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
